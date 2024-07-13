ee1361589b8c36f6e61f0638ad42eddd1f44377b
Jude Biose <judebiose20@gmail.com>
2024-07-13 21:33:49 +0100

    partial checkout

--- src/context/ProductsProvider.tsx
+++ src/context/ProductsProvider.tsx
@@ -62,7 +62,7 @@ const useProductContext = (initProductState: ProductStateType) => {
     getProducts().then((products) => {
       setProductResponse(products);
       setProducts(products?.items.map((product) => {
-        return { ...product, current_price: product?.current_price[0]?.NGN[0] as number }
+        return { ...product, current_price: product?.current_price[0]?.NGN[0] as number } /* eslint-disable  @typescript-eslint/no-explicit-any */
       }));
       setPageLoading(false);
       setTotalPagination(Math.ceil(products.total / products.size));
@@ -90,7 +90,7 @@ const useProductContext = (initProductState: ProductStateType) => {
     fetchProducts(payload).then((products) => {
       setProductResponse(products);
       setProducts(products?.items.map((product) => {
-        return { ...product, current_price: product?.current_price[0]?.NGN[0] as number }
+        return { ...product, current_price: product?.current_price[0]?.NGN[0] as number } /* eslint-disable  @typescript-eslint/no-explicit-any */
       }));
       setPageLoading(false);
       setTotalPagination(Math.ceil(products.total / products.size));
